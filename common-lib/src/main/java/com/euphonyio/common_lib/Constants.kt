a7394da81df2704be0fa38d4c5ef00a6009502c4
SeonJK <tjswhdrbs14@naver.com>
2022-09-02 01:11:03 +0900

    [feature] Add common-lib for boilerplate code #1
    - Add common-library
    - Add Constants class for frequency number in common-lib
    - Add euphony library in common-lib
    - Add RECORD_AUDIO permission in common-lib

---
+++ common-lib/src/main/java/com/euphonyio/common_lib/Constants.kt
@@ -0,0 +1,14 @@
+package com.euphonyio.common_lib
+
+/**
+ * eupi-camera
+ * Created by SeonJK
+ * Date: 2022-09-02
+ * Time: 오전 12:31
+ * */
+class Constants {
+    companion object {
+        const val FREQUENCY_CAPTURE: Int = 18000
+        const val FREQUENCY_SWITCH_CAMERA: Int = 21000
+    }
+}
