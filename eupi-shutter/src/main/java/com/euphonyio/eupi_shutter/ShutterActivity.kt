8b1a403bdc613c9c57db299aaa3b24d2a74ebba4
유나연 <yny026@gmail.com>
2022-09-03 15:43:17 +0900

    layout:셔터, 전환 버튼 추가

--- eupi-shutter/src/main/java/com/euphonyio/eupi_shutter/ShutterActivity.kt
+++ eupi-shutter/src/main/java/com/euphonyio/eupi_shutter/ShutterActivity.kt
@@ -3,14 +3,19 @@ package com.euphonyio.eupi_shutter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.euphonyio.eupi_shutter.ui.theme.EupicameraTheme
+import com.euphonyio.eupi_shutter.ui.theme.Purple200
 
 class ShutterActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +25,7 @@ class ShutterActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-                    Greeting("Shutter")
+                    ShutterPreview()
                 }
             }
         }
@@ -28,14 +33,35 @@ class ShutterActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CameraChange(str: String){
+    Button(
+        onClick = {  },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Purple200
+        ),
+        shape = RoundedCornerShape(24.dp)
+    ) {
+        Text(
+            text = str,
+            fontSize = 18.sp,
+            color = colorResource(id = R.color.white),
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    EupicameraTheme {
-        Greeting("Android")
+fun ShutterPreview() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        CameraChange("Shutter")
+        Spacer(modifier = Modifier.height(100.dp))
+        CameraChange("Change")
     }
 }
