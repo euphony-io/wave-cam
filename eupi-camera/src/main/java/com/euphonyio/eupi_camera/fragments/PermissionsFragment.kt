16ec86b12d47323a9a684a37e555dc8765ffc539
SeonJK <tjswhdrbs14@naver.com>
2022-09-15 01:08:11 +0900

    [feature] migration/apply registerForActivityResult

--- eupi-camera/src/main/java/com/euphonyio/eupi_camera/fragments/PermissionsFragment.kt
+++ eupi-camera/src/main/java/com/euphonyio/eupi_camera/fragments/PermissionsFragment.kt
@@ -21,6 +21,8 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -28,7 +30,8 @@ import androidx.navigation.Navigation
 import com.euphonyio.eupi_camera.R
 
 private const val PERMISSIONS_REQUEST_CODE = 10
-private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)
+private val PERMISSIONS_REQUIRED =
+    arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
 
 /**
  * The sole purpose of this fragment is to request permissions and, once granted, display the
@@ -41,31 +44,33 @@ class PermissionsFragment : Fragment() {
 
         if (!hasPermissions(requireContext())) {
             // Request camera-related permissions
-            requestPermissions(PERMISSIONS_REQUIRED, PERMISSIONS_REQUEST_CODE)
+            requestPermissionAction()
         } else {
             // If permissions have already been granted, proceed
             navigateToCamera()
         }
+
+
     }
 
-    override fun onRequestPermissionsResult(
-            requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSIONS_REQUEST_CODE) {
-            if (PackageManager.PERMISSION_GRANTED == grantResults.firstOrNull()) {
-                // Take the user to the success fragment when permission is granted
-                Toast.makeText(context, "Permission request granted", Toast.LENGTH_LONG).show()
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions: Map<String, Boolean> ->
+            if(!permissions.containsValue(false)) {
                 navigateToCamera()
             } else {
                 Toast.makeText(context, "Permission request denied", Toast.LENGTH_LONG).show()
             }
         }
+
+    private fun requestPermissionAction() {
+        requestPermissionLauncher.launch(PERMISSIONS_REQUIRED)
     }
 
     private fun navigateToCamera() {
         lifecycleScope.launchWhenStarted {
             Navigation.findNavController(requireActivity(), R.id.fragment_container).navigate(
-                    PermissionsFragmentDirections.actionPermissionsToCamera())
+                PermissionsFragmentDirections.actionPermissionsToCamera()
+            )
         }
     }
 
